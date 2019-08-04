0edff69a38564963748e838d3acf0d38692f3ee2
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-08-04 21:16:03 +0200

    Remove leftover bulma classes. Add reference image.

--- src/components/timeline/timeline.vue
+++ src/components/timeline/timeline.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="timeline">
     <div class="timeline__content">
-      <div class="timeline__content__item columns" v-for="(item, index) in timelineItems" :key="index">
-        <div class="column is-mobile timeline__content__item__image">
+      <div class="timeline__content__item" v-for="(item, index) in timelineItems" :key="index">
+        <div class="timeline__content__item__image">
           <div class="timeline__content__item__image__asset" :style="{ backgroundImage: `url('${item.image}')` }"></div>
         </div>
-        <div class="column is-mobile">
+        <div class="timeline__content__item__text">
           <div class="timeline__content__item__title">{{ item.title }}</div>
           <div class="timeline__content__item__description">{{ item.description }}</div>
         </div>
