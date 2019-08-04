a23cd08a77817023c7d3f311e8a307a207c4c981
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-08-04 21:33:10 +0200

    Add header to timeline. Cleanup BEM structure.

--- src/components/timeline/timeline.vue
+++ src/components/timeline/timeline.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="timeline">
     <div class="timeline__content">
+      <div class="timeline__content__header">
+        <h2 class="timeline__content__header__title">Saturday 21st March 2020</h2>
+      </div>
+
       <div class="timeline__content__item" v-for="(item, index) in timelineItems" :key="index">
         <div class="timeline__content__item__image">
           <div class="timeline__content__item__image__asset" :style="{ backgroundImage: `url('${item.image}')` }"></div>
         </div>
         <div class="timeline__content__item__text">
           <div class="timeline__content__item__title">{{ item.title }}</div>
-          <div class="timeline__content__item__description">{{ item.description }}</div>
+          <div class="timeline__content__item__description" v-html="item.description"></div>
         </div>
       </div>
     </div>
