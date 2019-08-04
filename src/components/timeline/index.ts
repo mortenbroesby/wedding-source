35f0afa3bad2c7c7c8d578ea17ca630cba6e0258
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-08-04 21:13:39 +0200

    Add CSS reset. Add timeline DOM structure. Pretty up countdown.

--- src/components/timeline/index.ts
+++ src/components/timeline/index.ts
@@ -9,5 +9,33 @@ import "./timeline.scss";
   components: {}
 })
 export default class Timeline extends Vue {
-  
+  /*************************************************/
+  /* PROPERTIES */
+  /*************************************************/
+  timelineItems: any = {};
+
+  /*************************************************/
+  /* LIFE CYCLE */
+  /*************************************************/
+  mounted() {
+    this.fillItems();
+  }
+
+  /*************************************************/
+  /* METHODS */
+  /*************************************************/
+  fillItems() {
+    const items = new Array(10).fill(null).map((item, index) => {
+      return {
+        id: index,
+        image: require("../../assets/cover.jpg"),
+        title: "Title 1",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id fermentum arcu. 
+          Mauris rhoncus libero ac mauris facilisis faucibus. Quisque sodales malesuada massa at sollicitudin. 
+          Morbi eget finibus dui, vel tempor elit. Ut bibendum nec mauris sed congue. Donec blandit augue eros,`
+      }
+    })
+
+    this.timelineItems = items;
+  }
 }
--- src/components/timeline/timeline.vue
+++ src/components/timeline/timeline.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="timeline">
     <div class="timeline__content">
-      
+      <div class="timeline__content__item columns" v-for="(item, index) in timelineItems" :key="index">
+        <div class="column is-mobile timeline__content__item__image">
+          <div class="timeline__content__item__image__asset" :style="{ backgroundImage: `url('${item.image}')` }"></div>
+        </div>
+        <div class="column is-mobile">
+          <div class="timeline__content__item__title">{{ item.title }}</div>
+          <div class="timeline__content__item__description">{{ item.description }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
