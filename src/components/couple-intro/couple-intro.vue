3e023f1249f9218ef5d4257c6be1543a0fb9dddd
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2019-08-09 17:16:25 +0200

    Switch font type. Add DOM structure to couple info.

--- src/components/couple-intro/couple-intro.vue
+++ src/components/couple-intro/couple-intro.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="couple-intro">
     <div class="couple-intro__couples">
-      <div class="couple-intro__couples__couple">
+      <div class="couple-intro__couples__couple couple-intro__couples__couple--bride"></div>
+      <div class="couple-intro__couples__and">&</div>
+      <div class="couple-intro__couples__couple couple-intro__couples__couple--groom"></div>
+    </div>
 
-      </div>
-
-      <div class="couple-intro__couples__couple">
-
-      </div>
+    <div class="couple-intro__text">
+      <h1 class="couple-intro__text__title">{{ $t("couple-intro.title") }}</h1>
+      <h3 class="couple-intro__text__description">{{ $t("couple-intro.description") }}</h3>
     </div>
   </div>
 </template>
